fd478208ca0259e35881252c82462d422918bb69
Carol McMasters-Stone <cbeck@ucdavis.edu>
2017-07-18 11:05:45 -0700

    close to working

--- src/webcouturier/dropdownmenu/browser/dropdown.js
+++ src/webcouturier/dropdownmenu/browser/dropdown.js
@@ -1,8 +1,18 @@
 /* The following line defines global variables defined elsewhere. */
 /*globals $, jQuery*/
 
-jQuery(function ($) {
-    $('#portal-globalnav .noClick').click(function (e) {
-        e.preventDefault();
-    });
-});
+
+if (jQuery) {
+    (function ($) {
+        "use strict";
+        $(document).ready(function () {
+            // initialize the megamenu
+            $('.megamenu').accessibleMegaMenu();
+            
+            // hack so that the megamenu doesn't show flash of css animation after the page loads.
+            setTimeout(function () {
+                $('body').removeClass('init');
+            }, 500);
+        });
+    }(jQuery));
+}
